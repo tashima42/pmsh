d23fd0f2ee41c416a1b58a23f6b2f4f19a15641f
Pedro Tashima <pedrotashima42@gmail.com>
2021-10-09 10:15:43 -0300

    updated install script

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Simple project manager CLI for GNU/Linux
 ## Installation
 **Warning**: always inspect scripts before running them, if you're not comfortable installing this way, just follow line by line the `install.sh` script
 ```bash
-curl https://raw.githubusercontent.com/Tashima42/pmsh/main/install.sh | sudo sh
+curl https://raw.githubusercontent.com/Tashima42/pmsh/main/install.sh | sh
 ```
 Uninstall:
 ```bash
--- install.sh
+++ install.sh
@@ -13,20 +13,20 @@ source_line="source \$HOME/.config/.pmsh"
 case $SHELL in
 */zsh) 
   echo "===Using ZSH==="
-  sh -c "echo $source_line | tee -a $HOME/.zshrc"
+  echo $source_line >> $HOME/.zshrc
   echo "===added '$source_line' to .zshrc==="
   echo "RUN: 'source ~/.zshrc'"
   ;;
 */bash)
   echo "===Using BASH==="
-  sh -c "echo $source_line | tee -a $HOME/.bashrc"
+  echo $source_line >> $HOME/.bashrc
   echo "===added '$source_line' to .bashrc==="
   echo "RUN: 'source ~/.bashrc'"
   ;;
 *)
   echo "WARNING: couldn't append to your rc file, please add it manually" 
-  echo "run: '$source_line | tee -a $HOME/.bashrc'"
+  echo "run: '$source_line >> $HOME/.bashrc'"
   echo "OR"
-  echo "run: '$source_line | tee -a $HOME/.zshrc'"
+  echo "run: '$source_line >> $HOME/.zshrc'"
 esac
 
